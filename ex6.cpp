4d4f4217445df6ff2004f005a558de6796425fa4
yunngaeng <yungyeong04@gmail.com>
2024-12-02 23:31:12 +0900

    Add files via upload

---
+++ ex6.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void menu(void);
+int add(int x, int y);
+int sub(int x, int y);
+int mul(int x, int y);
+int div(int x, int y);
+
+void menu(void)
+{
+	printf("=====================\n");
+	printf("0. 덧셈\n");
+	printf("1. 뺄셈\n");
+	printf("2. 곱셈\n");
+	printf("3. 나눗셈\n");
+	printf("4. 종료\n");
+	printf("=====================\n");
+}
+
+int main(void)
+{
+	int choice, result, x, y;
+	int(*pf[4])(int, int) = { add, sub, mul, div };
+
+	while (1)
+	{
+		menu();
+		printf("메뉴를 선택하시오:");
+		scanf_s("%d", &choice);
+
+		if (choice < 0 || choice >= 4)
+			break;
+		printf("2개의 정수를 입력하시오:");
+		scanf_s("%d %d", &x, &y);
+
+		result = pf[choice](x, y);
+		printf("연산 결과 = %d\n", result);
+	}
+	return 0;
+}
+
+int add(int x, int y)
+{
+	return x + y;
+}
+
+int sub(int x, int y)
+{
+	return x - y;
+}
+
+int mul(int x, int y)
+{
+	return x * y;
+}
+
+int div(int x, int y)
+{
+	return x / y;
+}
---
+++ ex9.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	char s[] = "Barking dogs seldom bite.";
+	char t[] = "A bad workman blames his tools";
+	const char* p = s;
+	char* const q = s;
+
+	p = t;
+
+	q[3] = 'a';
+
+		//q = t;
+
+		return 0;
+}
